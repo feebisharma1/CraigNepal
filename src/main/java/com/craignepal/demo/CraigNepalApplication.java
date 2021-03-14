e7f39e35cac55f7a09c937d68d0b008a15356658
feebisharma1 <jesuslovesfeebi@gmail.com>
2021-03-14 16:35:28 -0500

    Added spring boot template

---
+++ src/main/java/com/craignepal/demo/CraigNepalApplication.java
@@ -0,0 +1,13 @@
+package com.craignepal.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CraigNepalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CraigNepalApplication.class, args);
+	}
+
+}
